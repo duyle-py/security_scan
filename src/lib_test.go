7761941266afb2c2655d9577f8cd0e60884126aa
Duy Le <duyltk.cs@gmail.com>
2022-11-20 16:50:05 +0700

    add unit test for finding words func

--- src/lib_test.go
+++ src/lib_test.go
@@ -1,9 +1,33 @@
 package src
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
-func TestApp(t *testing.T) {
-	FindWords("/tmp/repo771652293/react")
+func TestGrep(t *testing.T) {
+	dir, err := ioutil.TempDir("/tmp", "sec")
+	if err != nil {
+		t.Error(err)
+	}
+	f, err := os.Create(dir + "/sec_unit_test.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+
+	_, err = f.WriteString("public_key \n private_key")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	findings, err := FindWords(dir)
+
+	if len(findings) != 2 {
+		t.Error("Grep is failed in finding words")
+	}
+	f.Close()
+	os.RemoveAll(dir)
 }
